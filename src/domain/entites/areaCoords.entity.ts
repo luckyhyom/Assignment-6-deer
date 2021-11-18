cdfc18f94f571e85f59de4b1179ec774ee4178bf
heejin99 <wkdgmlwls30@naver.com>
2021-11-19 00:34:33 +0900

    Chore: Entity 컬럼 추가
    - Area Coords
    - Forbidden Area Zones
    - Forbidden Coords

--- src/domain/entites/areaCoords.entity.ts
+++ src/domain/entites/areaCoords.entity.ts
@@ -1,6 +1,23 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
+@Entity("area_coords")
 export class AreaCoords {
+    @PrimaryGeneratedColumn({ name: "area_coords", type: "int"})
+    area_coords!: number;
+    
+    @Column({type: "decimal", precision: 16, scale: 14, default: 0, nullable: false})
+    area_coords_lat!: number;
 
+    @Column({type: "decimal", precision: 17, scale: 14, default: 0, nullable: false})
+    area_coords_lng!: number;
+
+    @CreateDateColumn({default: Date.now(), nullable: false})
+    createdAt!: Date;
+
+    @UpdateDateColumn({default: Date.now(), nullable: false})
+    updatedAt!: Date;
+ 
+    @ManyToOne(() => Areas, (area)=>area.area_id)
+    area_id?: Areas;
+    
 }
--- src/domain/entites/forbiddenCoords.entity.ts
+++ src/domain/entites/forbiddenCoords.entity.ts
@@ -1,6 +1,24 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { ForbiddenAreaZones } from "./forbiddenAreaZones.entity";
 
-@Entity()
+@Entity("forbidden_coords")
 export class ForbiddenCoords {
+    @PrimaryGeneratedColumn({ name: "forbidden_coords_id", type: "int"})
+    forbidden_coords_id!: number;
+    
+    @Column({type: "decimal", precision: 20, scale: 2, default: 0, nullable: false})
+    forbidden_coords_lat!: number;
 
+    @Column({type: "decimal", precision: 20, scale: 2, default: 0, nullable: false})
+    forbidden_coords_lng!: number;
+
+    @CreateDateColumn({default: Date.now(), nullable: false})
+    createdAt!: Date;
+
+    @UpdateDateColumn({default: Date.now(), nullable: false})
+    updatedAt!: Date;
+ 
+    @ManyToOne(() => ForbiddenAreaZones, (forbiddenAreaZones) => forbiddenAreaZones.forbidden_area_id)
+    forbidden_area_id?: ForbiddenAreaZones;
+    
 }
